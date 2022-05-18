0cb780abcd420d9f93323708877822578bf01671
Ritika Bhosale <54773240+ritikabhosale@users.noreply.github.com>
2022-05-18 23:08:32 +0530

    Added test file for webpage Generation

---
+++ test/testWebpageGeneration.js
@@ -0,0 +1,77 @@
+const assert = require('assert');
+const { emptyRow, emptyRows, generateTag, generateLetter, generateWord } = require('../src/webpageGeneration.js');
+
+describe('emptyRow', () => {
+  it('should create a row of a cell', () => {
+    const expected = '<div class="word"><div></div></div>';
+    assert.deepStrictEqual(emptyRow(1), expected);
+  });
+
+  it('should create a row of two cells', () => {
+    const expected = '<div class="word"><div></div><div></div></div>';
+    assert.deepStrictEqual(emptyRow(2), expected);
+  });
+
+  it('should create a row of no cells', () => {
+    const expected = '<div class="word"></div>';
+    assert.deepStrictEqual(emptyRow(0), expected);
+  });
+});
+
+describe('emptyRows', () => {
+  it('should create a empty row', () => {
+    const expected = ['<div class="word"><div></div><div></div><div></div><div></div><div></div></div>'];
+    assert.deepStrictEqual(emptyRows(1), expected);
+  });
+
+  it('should create two empty row', () => {
+    const expected = ['<div class="word"><div></div><div></div><div></div><div></div><div></div></div>', '<div class="word"><div></div><div></div><div></div><div></div><div></div></div>'];
+    assert.deepStrictEqual(emptyRows(2), expected);
+  });
+
+  it('should create no rows', () => {
+    const expected = [];
+    assert.deepStrictEqual(emptyRows(0), expected);
+  });
+});
+
+describe('generateLetter', () => {
+  it('should generate tag with given content and class', () => {
+    const expected = '<div class="present">a</div>';
+    assert.deepStrictEqual(generateLetter(['a', 'present']), expected);
+  });
+
+  it('should generate a tag with given class and no content', () => {
+    const expected = '<div class="absent"></div>';
+    assert.deepStrictEqual(generateLetter(['', 'absent']), expected);
+  });
+
+  it('should generate a tag with given content and no class', () => {
+    const expected = '<div>a</div>';
+    assert.deepStrictEqual(generateLetter(['a', '']), expected);
+  });
+});
+
+describe('generateTag', () => {
+  it('should generate tag without class', () => {
+    const expected = '<p>hello</p>';
+    assert.deepStrictEqual(generateTag('p', 'hello', ''), expected);
+  });
+
+  it('should generate tag with class', () => {
+    const expected = '<p class="intro">hello</p>';
+    assert.deepStrictEqual(generateTag('p', 'hello', 'intro'), expected);
+  });
+});
+
+describe('generateWord', () => {
+  it('should create a 1 letter word with given class and letter', () => {
+    const expected = '<div class="word"><div class="present">a</div></div>'
+    assert.deepStrictEqual(generateWord([['a', 'present']]), expected);
+  });
+
+  it('should create a 2 letter word with given classes and letters', () => {
+    const expected = '<div class="word"><div class="present">a</div><div class="absent">n</div></div>'
+    assert.deepStrictEqual(generateWord([['a', 'present'], ['n', 'absent']]), expected);
+  });
+});
